94a84611ba18de70736b0df5c8445329d3a01810
Ajay Kumar <aky22896@gmail.com>
2023-07-09 23:34:55 +0530

    "added how to get from database"

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -5,9 +5,7 @@ import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -17,5 +15,15 @@ public class StudentController {
     public ResponseEntity<String> addStudent(@RequestBody Student student){
         String response = studentService.addStudent(student);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("get_student_by_id")
+    public ResponseEntity getStudentById(@RequestParam("rollNo") int rollNo){
+        Student student = studentService.getStudentById(rollNo);
+        if(student == null){
+            return new ResponseEntity<>("Invalid roll Number",HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(student,HttpStatus.FOUND);
     }
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -18,5 +21,24 @@ public class StudentService {
         // and will be able to add the object (student) into the database;
         studentRepository.save(student);
         return "Student added Successfully";
+    }
+
+    public Student getStudentById(int rollNo){
+
+        // find by id will help us to search a record based on the primary key,
+        // this is not necessary that we wll always get the object,
+        // sometimes we can also get the null value when it does not exists in database
+        // so Optional is an inbuilt class in java, which represents it might be null or it might not be null
+        // means if this roll no. exists in database then it will give the object of student
+        // and if the roll no. doesn't exists then it will give the null value
+       Optional<Student> optionalStudent =  studentRepository.findById(rollNo);
+
+       // if the student is present then return the student with get method of jpa interface
+       if(optionalStudent.isPresent()){
+           return optionalStudent.get();
+       }
+
+       // if it's not present then return the null
+       return  null;
     }
 }
